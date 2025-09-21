e352c2f2b6a868e0705009c1b2cf7717315edf8e
PaulAlejoR <chicarcas_par@hotmail.com>
2025-09-21 13:15:31 -0600

    Se agregaron todos los recursos de la pagina Slides

--- componentes/src/app/pages/slides/slides.page.ts
+++ componentes/src/app/pages/slides/slides.page.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Renderer2 } from "@angular/core";
+import { NavController } from "@ionic/angular";
 import { register } from "swiper/element/bundle";
+import { Swiper, SwiperOptions } from 'swiper/types';
+import { EffectCreative } from 'swiper/modules';
 
 register();
 @Component({
@@ -9,7 +12,38 @@ register();
   standalone: false
 })
 export class SlidesPage implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
+  slides: { img: string, titulo: string, desc: string }[] = [
+    {
+      img: '/assets/slides/photos.svg',
+      titulo: 'Comparte Fotos',
+      desc: 'Mira y comparte increíbles fotos de todo el mundo'
+    },
+    {
+      img: '/assets/slides/music-player-2.svg',
+      titulo: 'Escucha Música',
+      desc: 'Toda tu música favorita está aquí'
+    },
+    {
+      img: '/assets/slides/calendar.svg',
+      titulo: 'Nunca olvides nada',
+      desc: 'El mejor calendario del mundo a tu disposición'
+    },
+    {
+      img: '/assets/slides/placeholder-1.svg',
+      titulo: 'Tu ubicación',
+      desc: 'Siempre sabremos donde estás!'
+    }
+  ];
+
+  constructor( private navCtrl: NavController ) {}
+
+  ngOnInit() {
+    
+  }
+
+
+  onClick(){
+    this.navCtrl.navigateBack('/');
+  }
 }
